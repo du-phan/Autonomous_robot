b50f7d58b5f6f390b38b19b63c0357794a186393
Mr.Fab <mr.fab@live.com>
2016-12-05 20:22:44 +0100

    created and tested angle to dynamixel pos function Signed-off-by: Mr.Fab <mr.fab@live.com>

--- Code/basic_movement_test/basic_movement_test.ino
+++ Code/basic_movement_test/basic_movement_test.ino
@@ -1,6 +1,7 @@
 #define DXL_BUS_SERIAL1 1  //Dynamixel on Serial1(USART1)  <-OpenCM9.04
 
 #define ID_NUM 1
+#define DXL_POSITIONS_PER_DEGREE 1024.0/300.0
 
 Dynamixel Dxl(DXL_BUS_SERIAL1);
 
@@ -12,11 +13,21 @@ void setup() {
 }
 
 void loop() {
-  Dxl.goalPosition(ID_NUM, 0); //ID 1 dynamixel moves to position 1
+  Dxl.goalPosition(ID_NUM, dxlAngle(-90)); //ID 1 dynamixel moves to position 1
   delay(1000);
-  Dxl.goalPosition(ID_NUM, 500);//ID 1 dynamixel moves to position 1023
+  Dxl.goalPosition(ID_NUM, dxlAngle(-90));//ID 1 dynamixel moves to position 1023
   delay(1000);
 }
 
 
-
+int dxlAngle(float angleDEG)//returns the 0-1023 value needed to get this -90°~90° angle
+{
+  int anglePos = (358.4 + DXL_POSITIONS_PER_DEGREE * angleDEG);
+  
+  if(anglePos > 1023)//make sure we return a 0-1023 value
+    anglePos = 1023;
+  else if(anglePos < 0)
+    anglePos = 0;
+  
+  return anglePos;
+}
