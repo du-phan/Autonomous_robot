88ae4d5c67ce0909106001a65b3dee8bd6055960
Mr.Fab <mr.fab@live.com>
2016-12-09 12:47:26 +0100

    Signed-off-by: Mr.Fab <mr.fab@live.com>

--- Code/basic_movement_test/basic_movement_test.ino
+++ Code/basic_movement_test/basic_movement_test.ino
@@ -9,14 +9,32 @@ void setup() {
   // Initialize the dynamixel bus:
   // Dynamixel 2.0 Baudrate -> 0: 9600, 1: 57600, 2: 115200, 3: 1Mbps  
   Dxl.begin(3);  
-  Dxl.jointMode(ID_NUM); //jointMode() is to use position mode
+  Dxl.jointMode(1); //jointMode() is to use position mode
+  Dxl.jointMode(2); //jointMode() is to use position mode
+  
+  Dxl.goalPosition(1, dxlAngle(0));//ID 1 dynamixel moves to position 1023
+  
+  Dxl.goalPosition(2, dxlAngle(0));//ID 1 dynamixel moves to position 1023
 }
 
 void loop() {
-  Dxl.goalPosition(ID_NUM, dxlAngle(-90)); //ID 1 dynamixel moves to position 1
-  delay(1000);
-  Dxl.goalPosition(ID_NUM, dxlAngle(-90));//ID 1 dynamixel moves to position 1023
-  delay(1000);
+  for(int i = -45; i<45;i+=5)
+  {
+    Dxl.goalPosition(1, dxlAngle(i)); //ID 1 dynamixel moves to position 1
+    delay(300);
+  }
+  
+  Dxl.goalPosition(1, dxlAngle(0));//ID 1 dynamixel moves to position 1023
+  delay(500);
+  
+  for(int i = -45; i<45;i+=5)
+  {
+    Dxl.goalPosition(2, dxlAngle(i)); //ID 1 dynamixel moves to position 1
+    delay(300);
+  }
+  
+  Dxl.goalPosition(2, dxlAngle(0));//ID 1 dynamixel moves to position 1023
+  delay(500);
 }
 
 
